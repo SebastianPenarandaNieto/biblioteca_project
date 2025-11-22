83d3884af1daf20b6b8fe78afbf319d1ea4b0074
Sebastián Peñaranda <sebaspenieto@gmail.com>
2025-11-22 11:05:08 -0500

    Incorporación de los modelos "in line" en el admin para una mejor visualización de las relaciones.

--- biblioteca/admin.py
+++ biblioteca/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Autor, Libro, Resena
 
+
+class LibroInLine(admin.TabularInline):
+    model = Libro
+    extra = 0
+
+class ResenaInLine(admin.TabularInline):
+    model = Resena
+    extra = 0
+
 @admin.register(Autor)
 class AutorAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'nacionalidad')
@@ -8,6 +17,7 @@ class AutorAdmin(admin.ModelAdmin):
     list_filter = ('nombre', 'nacionalidad')
     ordering = ('nombre',)
     list_per_page = 20
+    inlines = [LibroInLine]
 
 @admin.register(Libro)
 class LibroAdmin(admin.ModelAdmin):
@@ -17,6 +27,7 @@ class LibroAdmin(admin.ModelAdmin):
     ordering = ('autor', '-fecha_publicacion')
     list_per_page = 20
     date_hierarchy = "fecha_publicacion"
+    inlines = [ResenaInLine]
 
 
 @admin.register(Resena)
@@ -26,4 +37,3 @@ class ResenaAdmin(admin.ModelAdmin):
     list_filter = ('calificacion',)
     ordering = ('-calificacion',)
     list_per_page = 20
-
