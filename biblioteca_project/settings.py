5255d136fdd0cf11bcf048b25d74731bec1965a3
Sebastián Peñaranda <sebaspenieto@gmail.com>
2025-11-23 18:36:57 -0500

    Añadiendo configuraciones de django rest framework

--- biblioteca_project/settings.py
+++ biblioteca_project/settings.py
@@ -37,8 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'biblioteca'
+    'biblioteca',
+    'rest_framework',
+    'django_filters'
+    
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
