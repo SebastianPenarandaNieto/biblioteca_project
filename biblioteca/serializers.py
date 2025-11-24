6d51bc9aaeb14db9b7f87b63ab1105c26905b336
Sebastián Peñaranda <sebaspenieto@gmail.com>
2025-11-23 20:10:26 -0500

    Incorporación de serializadores para cada modelo

---
+++ biblioteca/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from .models import *
+
+class AutorSerializer(serializers.ModelSerializer):
+    libros_recientes = serializers.SerializerMethodField()
+
+    def get_libros_recientes(self, obj):
+        libros = obj.libros.order_by('-fecha_publicacion')[:5]
+        return LibroSerializer(libros, many=True).data
+
+    class Meta:
+        model = Autor
+        fields = ['id', 'nombre', 'nacionalidad', 'libros_recientes']
+
+    
+class LibroSerializer(serializers.ModelSerializer):
+
+    resenas_recientes = serializers.SerializerMethodField()
+    nombre_autor = serializers.ReadOnlyField(source='autor.nombre')
+
+    def get_resenas_recientes(self, obj):
+        resenas = obj.resenas.order_by('-fecha')[:5]
+        return ResenaSerializer(resenas, many=True).data
+
+    class Meta:
+        model = Libro
+        fields = ['id', 'titulo', 'autor', 'nombre_autor', 'fecha_publicacion', 'resenas_recientes']
+
+
+
+class ResenaSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Resena
+        fields = ['id', 'libro', 'texto', 'calificacion', 'fecha']
+
+    
