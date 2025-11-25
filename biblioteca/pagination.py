64e77ecf25fc4866126d06e1f6dab336e4058113
Sebastián Peñaranda <sebaspenieto@gmail.com>
2025-11-25 01:12:19 -0500

    Detalles en la paginación

--- biblioteca/pagination.py
+++ biblioteca/pagination.py
@@ -2,14 +2,14 @@ from rest_framework.pagination import PageNumberPagination
 
 class AutorPagination(PageNumberPagination):
     page_size = 5
-    page_size_query_param = 'page'
+    page_size_query_param = 'page_size'
     max_page_size = 100
 
 class LibroPagination(PageNumberPagination):
     page_size = 5
-    page_size_query_param = 'page'
+    page_size_query_param = 'page_size'
     max_page_size = 100
 
 class ResenaPagination(PageNumberPagination):
-    page_size_query_param = 'page'
+    page_size_query_param = 'page_size'
     max_page_size = 100
